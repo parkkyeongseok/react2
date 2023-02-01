1d5e7f307cdd69281b5672c69b7e7a10c4be7c30
kyeongseok <andrewpark1116@naver.com>
2023-02-01 10:59:31 +0900

    src

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> 진짜 되는건가
+          Edit 진짜 되는건가 진짜진짜
         </p>
         <a
           className="App-link"
